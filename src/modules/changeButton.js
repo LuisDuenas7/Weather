49c7adb09d8a368fafa99a965858b02ef002d8e1
Luis Dueñas <99936957+LuisDuenas7@users.noreply.github.com>
2022-04-08 15:19:11 -0500

    U

--- src/modules/changeButton.js
+++ src/modules/changeButton.js
@@ -6,7 +6,7 @@ const ChangeButton = ({weather,icon}) => {
   
   return (
     <div className='temp-position'>
-        <img className="img-icon" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt='Img'/>
+        <img className="img-icon" src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt='Img'/>
       <h2 className='temp_value'> {celsious ?  weather.toFixed(1) :  weather.toFixed(0) * 9/5 + 32} {celsious ? '°C' : '°F'}</h2>  
       <button  onClick={() => setCelsious(!celsious)}>Change to {celsious? '°F' : '°C'}</button>
     </div>
